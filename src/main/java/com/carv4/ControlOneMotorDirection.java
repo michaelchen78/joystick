a81e1405d0296ccddbe0e40e88a66636fd50ed2c
michaelchen78 <56234424+michaelchen78@users.noreply.github.com>
2022-05-16 09:05:52 -0600

    Update ControlOneMotorDirection.java

--- src/main/java/com/carv4/ControlOneMotorDirection.java
+++ src/main/java/com/carv4/ControlOneMotorDirection.java
@@ -4,6 +4,7 @@ import com.pi4j.io.gpio.GpioController;
         import com.pi4j.io.gpio.GpioFactory;
         import com.pi4j.io.gpio.GpioPinDigitalOutput;
         import com.pi4j.io.gpio.RaspiPin;
+        import come.carv4.joystick;
 
 /**
  * Controls motor direction of a two DC motors
