00d31561416d5f16178fcac9c787d3d8d993a437
michaelchen78 <56234424+michaelchen78@users.noreply.github.com>
2022-05-16 08:42:53 -0600

    Add files via upload

---
+++ assets/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java --module-path . --module carv4/com.carv4.ControlOneMotorDirection $@
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module carv4 {
+    requires pi4j.core;
+
+    opens com.carv4 to com.pi4j;
+}
