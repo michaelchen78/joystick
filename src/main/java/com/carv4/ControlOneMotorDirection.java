004de3a85133052fdc8cc6a1e991e74060815615
michaelchen78 <56234424+michaelchen78@users.noreply.github.com>
2022-05-16 08:43:58 -0600

    Update ControlOneMotorDirection.java

--- src/main/java/com/carv4/ControlOneMotorDirection.java
+++ src/main/java/com/carv4/ControlOneMotorDirection.java
@@ -12,44 +12,46 @@ import com.pi4j.io.gpio.GpioController;
 public class ControlOneMotorDirection {
 
     public static void main(String[] args) throws InterruptedException {
-        // get a handle to the GPIO controller
-        final GpioController gpio = GpioFactory.getInstance();
+                //Initzalize the joystick component
+        final var joystick = new Joystick(pi4j, 5,6,13,19,26);
 
-        final GpioPinDigitalOutput motor1pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "m1A");
-        final GpioPinDigitalOutput motor1pinB = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "m1B");
-        final GpioPinDigitalOutput motor1pinE = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "m1E");
-        final GpioPinDigitalOutput motor2pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "m2A");
-        final GpioPinDigitalOutput motor2pinB = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "m2B");
-        final GpioPinDigitalOutput motor2pinE = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "m2E");
+        //Register event handlers to print a message when pressed (onDown) and (onUp)
+        joystick.buttonUpOnDown(() -> System.out.println("Pressing joystick button UP"));
+        joystick.buttonUpOnUp(() -> System.out.println("Stop pressing joystick button UP"));
 
-        System.out.println("rotate both motors clockwise for 3 seconds");
-        motor1pinA.high();
-        motor1pinB.low();
-        motor1pinE.high();
-        motor2pinA.high();
-        motor2pinB.low();
-        motor2pinE.high();
+        joystick.buttonLeftOnDown(() -> System.out.println("Pressing joystick button LEFT"));
+        joystick.buttonLeftOnUp(() -> System.out.println("Stop pressing joystick button LEFT"));
 
-        // wait 3 seconds
-        Thread.sleep(3000);
+        joystick.buttonDownOnDown(() -> System.out.println("Pressing joystick button DOWN"));
+        joystick.buttonDownOnUp(() -> System.out.println("Stop pressing joystick button DOWN"));
 
-        System.out.println("rotate motor 1 in oposite derection for 3 seconds "
-                + "and motor 2 in oposite direction for 6 seconds");
-        motor1pinA.low();
-        motor1pinB.high();
-        motor2pinA.low();
-        motor2pinB.high();
+        joystick.buttonRightOnDown(() -> System.out.println("Pressing joystick button RIGHT"));
+        joystick.buttonRightOnUp(() -> System.out.println("Stop pressing joystick button RIGHT"));
 
-        // wait 3 seconds
-        Thread.sleep(3000);
-        System.out.println("Stopping motor 1");
-        motor1pinE.low();
+        joystick.buttonPushOnDown(() -> System.out.println("Pressing joystick button PUSH"));
+        joystick.buttonPushOnUp(() -> System.out.println("Stop pressing joystick button PUSH"));
 
-        // wait 3 seconds
-        Thread.sleep(3000);
-        System.out.println("Stopping motor 2");
-        motor2pinE.low();
+        // Wait for 15 seconds while handling events before exiting
+        System.out.println("Press the button to see it in action!");
+        sleep(15000);
 
-        gpio.shutdown();
+        // Unregister all event handlers to exit this application in a clean way
+        joystick.buttonUpOnDown(null);
+        joystick.buttonUpOnUp(null);
+
+        joystick.buttonLeftOnDown(null);
+        joystick.buttonLeftOnUp(null);
+
+        joystick.buttonDownOnDown(null);
+        joystick.buttonDownOnUp(null);
+
+        joystick.buttonRightOnDown(null);
+        joystick.buttonRightOnUp(null);
+
+        joystick.buttonPushOnDown(null);
+        joystick.buttonPushOnUp(null);
+
+        joystick.buttonUpOnDown(null);
+        joystick.buttonUpOnUp(null);
     }
 }
